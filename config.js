7f9d77de470da4e4975adba1a24af45486978a77
Dubong-hedgehog <ljh1004sms@gmail.com>
2026-08-23 10:29:06 +0900

    Add Firebase configuration to config.js
    Added Firebase configuration object for project.

--- config.js
+++ config.js
@@ -2,3 +2,13 @@ const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbxkzen_xKJvEY9EBIPXeVqwUY1Cz1t4VO0wO3a3MXED5GJVjqtzMZKwJvW1oJUA7AwhGQ/exec',
   GENERATION: '8'
 };
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDRlgN66yBecmJvcNVZuVhduQHsz10vGbU",
+  authDomain: "sdm-cardbattle.firebaseapp.com",
+  databaseURL: "https://sdm-cardbattle-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sdm-cardbattle",
+  storageBucket: "sdm-cardbattle.firebasestorage.app",
+  messagingSenderId: "536578905514",
+  appId: "1:536578905514:web:9c3da5ec72ddd24a2e47d3"
+};
